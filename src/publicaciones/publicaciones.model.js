5e651bc60a9f8961f6d534e81ea50dde2b82f99f
danitt030 <danielitotuchez100@gmail.com>
2025-02-20 22:18:02 -0600

    Modificacion en la categoria por defecto al momento de crear se duplicaba, ahora ya no

---
+++ src/publicaciones/publicaciones.model.js
@@ -0,0 +1,28 @@
+import { Schema, model } from "mongoose";
+
+const PublicacionSchema = new Schema({
+    titulo: {
+        type: String,
+        required: [true, "El titulo es obligatorio"]
+    },
+    texto: {
+        type: String,
+        required: [true, "El texto es obligatorio"],
+        maxLength: [250, "El texto no debe exceder los 250 caracteres"]
+    },
+    categoria: {
+        type: Schema.Types.ObjectId,
+        ref: "Categoria",
+        required: [true, "La categoria es obligatoria"]
+    },
+    autor: {
+        type: Schema.Types.ObjectId,
+        ref: "Usuario",
+        required: [true, "El autor es obligatorio"]
+    },
+}, {
+    versionKey: false,
+    timestamps: true
+});
+
+export default model("Publicacion", PublicacionSchema);
